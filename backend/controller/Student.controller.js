06091ceb087ee53657cf285ac41903bf7cc442a2
AYUSH <89582411+Ayush0031@users.noreply.github.com>
2024-05-29 23:01:37 +0530

    delete student

--- backend/controller/Student.controller.js
+++ backend/controller/Student.controller.js
@@ -7,6 +7,14 @@ const addStudent= async(req,res)=>{
         res.status(500).json({msg:"Not able to add Student"})
     }
 }
+const deleteStudent= async(req,res)=>{
+    try {
+        await Student.deleteOne(req.body._id)
+        res.status(201).json({msg:"Student deleted successfully"})
+    } catch (error) {
+        res.status(500).json({msg:"Error deleting student"})
+    }
+}
 const getAllStudent=async(req,res)=>{
     try {
         const data=await Student.find();
